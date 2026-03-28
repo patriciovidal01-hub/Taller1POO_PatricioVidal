f3904e62eaf6bf53db1d98628cacf31132ba4698
patri <patricio.vidal01@alumnos.ucn.cl>
2026-03-28 10:35:27 -0300

    Avance 1

--- src/taller1/Taller1.java
+++ src/taller1/Taller1.java
@@ -1,8 +1,25 @@
 package taller1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+
+
 public class Taller1 {
 
 	public static void main(String[] args){ 
+		// Patricio Javier Vidal Veas
+		// 22.330.827-9
+		// ICCI
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("???.txt"))) {
+			
+			
+		} catch (Exception e){
+			System.out.println("Error");
+		}
+		
 		System.out.println("pepe");
 		System.out.println("pepardo");
 		System.out.println("pepardo");
